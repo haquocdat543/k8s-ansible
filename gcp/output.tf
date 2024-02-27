4eaf233ab228cafa0fd9964420e7893bdbd6ebc2
haquocdat543 <wwwdatha543@gmail.com>
2024-02-27 19:33:16 +0700

    Add gcp

---
+++ gcp/output.tf
@@ -0,0 +1,40 @@
+output "loadbalancer-internal-ip" {
+  description = "Master private IP"
+  value       = google_compute_instance.loadbalancer.network_interface.0.network_ip
+}
+
+output "loadbalancer-external-ip" {
+  description = "Master public IP"
+  value       = google_compute_instance.loadbalancer.network_interface.0.access_config.0.nat_ip
+}
+
+output "master1-internal-ip" {
+  description = "Master private IP"
+  value       = google_compute_instance.master1.network_interface.0.network_ip
+}
+
+output "master1-external-ip" {
+  description = "Master public IP"
+  value       = google_compute_instance.master1.network_interface.0.access_config.0.nat_ip
+}
+
+output "master2-internal-ip" {
+  description = "Master private IP"
+  value       = google_compute_instance.master2.network_interface.0.network_ip
+}
+
+output "master2-external-ip" {
+  description = "Master public IP"
+  value       = google_compute_instance.master2.network_interface.0.access_config.0.nat_ip
+}
+
+output "worker1-internal-ip" {
+  description = "Master private IP"
+  value       = google_compute_instance.worker1.network_interface.0.network_ip
+}
+
+output "worker1-external-ip" {
+  description = "Master public IP"
+  value       = google_compute_instance.worker1.network_interface.0.access_config.0.nat_ip
+}
+
