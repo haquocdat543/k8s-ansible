b48b284abdf92beaf0fa3a9bd5589babcbd4d6dd
haquocdat543 <wwwdatha543@gmail.com>
2024-02-27 14:04:46 +0700

    Update ansible

--- ansible/all.sh
+++ ansible/all.sh
@@ -3,3 +3,5 @@
 ansible-playbook ./hosts.yaml -i ./inventory
 ansible-playbook ./loadbalancer.yaml -i ./inventory
 ansible-playbook ./init.yaml -i ./inventory
+ansible-playbook ./join-masters.yaml -i ./inventory
+ansible-playbook ./join-workers.yaml -i ./inventory
