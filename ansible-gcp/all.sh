3dc0418ae85939fec95dd4419fe4ccfa8c293783
haquocdat543 <wwwdatha543@gmail.com>
2024-02-28 09:10:57 +0700

    Add kubeconfig.yaml to ansible

--- ansible-gcp/all.sh
+++ ansible-gcp/all.sh
@@ -7,3 +7,4 @@ ansible-playbook ./init.yaml -i ./inventory
 ansible-playbook ./join-masters.yaml -i ./inventory
 ansible-playbook ./join-workers.yaml -i ./inventory
 ansible-playbook ./get_nodes.yaml -i ./inventory
+ansible-playbook ./kubeconfig.yaml -i ./inventory
