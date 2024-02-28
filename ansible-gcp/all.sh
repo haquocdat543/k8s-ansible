5708c9dff565406e7e46d1cd82e895a5d28fcec7
haquocdat543 <wwwdatha543@gmail.com>
2024-02-28 08:13:11 +0700

    Update ansible ( rename and add folder )

---
+++ ansible-gcp/all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ansible-playbook ./hosts.yaml -i ./inventory
+ansible-playbook ./loadbalancer.yaml -i ./inventory
+ansible-playbook ./init.yaml -i ./inventory
+ansible-playbook ./join-masters.yaml -i ./inventory
+ansible-playbook ./join-workers.yaml -i ./inventory
+ansible-playbook ./get_nodes.yaml -i ./inventory
