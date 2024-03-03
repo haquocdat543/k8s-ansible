2d01cd103a98a35ff0e507f566eba45d0457d845
haquocdat543 <wwwdatha543@gmail.com>
2024-03-03 16:47:56 +0700

    Update azure output

--- azure/output.tf
+++ azure/output.tf
@@ -1,12 +1,12 @@
 output "loadbalancer" {
-  value = azurerm_linux_virtual_machine.loadbalancer.public_ip_address
+  value       = "ssh -i $HOME/.ssh/id_rsa adminuser@${azurerm_linux_virtual_machine.loadbalancer.public_ip_address}"
 }
 
 output "master1" {
-  value = azurerm_linux_virtual_machine.master1.public_ip_address
+  value       = "ssh -i $HOME/.ssh/id_rsa adminuser@${azurerm_linux_virtual_machine.master1.public_ip_address}"
 }
 
 output "worker1" {
-  value = azurerm_linux_virtual_machine.worker1.public_ip_address
+  value       = "ssh -i $HOME/.ssh/id_rsa adminuser@${azurerm_linux_virtual_machine.worker1.public_ip_address}"
 }
 
